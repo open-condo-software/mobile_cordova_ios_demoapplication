042d0ba2d587348591111f041772dba9de528de8
alexandr.sivash <sanny199955@mail.ru>
2024-06-24 16:54:17 +0500

    added cordova-plugin-network-information

--- MainCordovaApplication/platforms/ios/platform_www/cordova_plugins.js
+++ MainCordovaApplication/platforms/ios/platform_www/cordova_plugins.js
@@ -242,12 +242,29 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       "clobbers": [
         "navigator.device.capture"
       ]
+    },
+    {
+      "id": "cordova-plugin-network-information.network",
+      "file": "plugins/cordova-plugin-network-information/www/network.js",
+      "pluginId": "cordova-plugin-network-information",
+      "clobbers": [
+        "navigator.connection"
+      ]
+    },
+    {
+      "id": "cordova-plugin-network-information.Connection",
+      "file": "plugins/cordova-plugin-network-information/www/Connection.js",
+      "pluginId": "cordova-plugin-network-information",
+      "clobbers": [
+        "Connection"
+      ]
     }
   ];
   module.exports.metadata = {
     "cordova-plugin-whitelist": "1.3.5",
     "cordova-plugin-condo": "0.0.1",
     "cordova-plugin-file": "8.1.0",
-    "cordova-plugin-media-capture": "5.0.1-dev"
+    "cordova-plugin-media-capture": "5.0.1-dev",
+    "cordova-plugin-network-information": "3.0.0"
   };
 });
